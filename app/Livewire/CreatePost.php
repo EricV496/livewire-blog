ebac16e96add0ab9a7540e0ae18a2d64741add97
eric <eric.vazquez496@gmail.com>
2024-10-16 21:32:03 -0600

    Validación Validación al crear un nuevo post

--- app/Livewire/CreatePost.php
+++ app/Livewire/CreatePost.php
@@ -10,12 +10,24 @@ class CreatePost extends Component
     public $isOpen = false;
     public $title, $content;
     
+    protected $rules = [
+        'title' => 'required|max:10',
+        'content' => 'required|min 100'
+    ];
+
     public function render()
     {
         return view('livewire.create-post');
     }
 
+    public function updated($propertyName){
+        $this->validateOnly($propertyName);
+    }
+
     public function save(){
+
+        $this->validate();
+
         Post::create([
             'title' => $this->title,
             'content' => $this->content,
